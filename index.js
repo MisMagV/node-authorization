dd8f4ceef0031bb0d822c69dd1d32890991c9433
Jeffrey Jen <yihungjen@gmail.com>
2016-01-07 16:51:25 +0000

    BEGIN: Demo app with login mechanism

---
+++ index.js
@@ -0,0 +1,25 @@
+"use strict"
+
+var express = require("express");
+
+var app = require("./lib/app");
+
+// Setup components assets
+app.use("/assets", express.static("html/bower_components/"));
+app.use("/assets", express.static("html/custom_components/"));
+
+// Setup static assets
+app.use("/css", express.static("html/www/css/"));
+app.use("/js", express.static("html/www/js/"));
+
+// Setup dynamic view repostitory
+app.set("views", "html/views/");
+
+// Register landing page router
+app.use("/", require("./lib/main").Main);
+
+var server = app.listen(4040, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Example app listening at http://%s:%s', host, port);
+});
