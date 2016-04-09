d1d9fad4b7b59f346eb94186d1e8f029c88f2971
Jeffrey Jen <yihungjen@gmail.com>
2016-04-09 13:14:26 +0000

    FIX: ServerError variable typo

--- api/v1/index.js
+++ api/v1/index.js
@@ -1,33 +1,19 @@
 "use strict"
 
 const express = require("express");
+
 const api = module.exports= express.Router();
 
-const model = require("../../model/v1");
-
-api.use(function register_klass(req, res, next) {
-    // pre load account module;
-    req.account = model.account.model;
-    req.jwt_options = req.app.locals.jwt_options;
-    next();
-});
+const ServerError = require("../../lib/errors/server-error");
 
 api.use("/login", require("./login"));
 api.use("/signup", require("./signup"));
 
-const generic_error_message = {
-    meta: {
-        message: "Somthing went wrong",
-        code: 503,
-        errorCode: undefined,
-        status: "fail",
-        methodName: undefined,
-    }
-};
-
 api.use(function error_handle(err, req, res, next) {
-    err = err || generic_error_message;
+    if (!(err instanceof ServerError)) {
+        err = new ServerError(err);
+    }
     console.error("error:", err);
     // Last ditch effort to send error message off to user
-    res.status(err.code || 503).json(err || {});
+    res.status(err.code).send(err.message);
 });
